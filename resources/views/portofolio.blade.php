bb60ba3297f328baa6c8f310c94bb1c5a107fd53
Neli <sayabaik549@gmail.com>
2022-09-21 17:02:49 +0700

    Initial commit

---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,224 @@
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Portofolio</title>
+
+ <link rel="stylesheet" href="/css/app.css">
+ <link rel="stylesheet" href="/css/my-style.css">
+
+ <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+ <link rel="shortcut icon" href="favicon.ico">
+
+ <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+
+ <!-- Animate.css -->
+ <link rel="stylesheet" href="css/animate.css">
+ <!-- Icomoon Icon Fonts-->
+ <link rel="stylesheet" href="css/icomoon.css">
+ <!-- Bootstrap  -->
+ <link rel="stylesheet" href="css/bootstrap.css">
+ <!-- Owl Carousel -->
+ <link rel="stylesheet" href="css/owl.carousel.min.css">
+ <link rel="stylesheet" href="css/owl.theme.default.min.css">
+ <!-- Magnific Popup -->
+ <link rel="stylesheet" href="css/magnific-popup.css">
+
+ <link rel="stylesheet" href="css/style.css">
+
+
+ <!-- Modernizr JS -->
+ <script src="js/modernizr-2.6.2.min.js"></script>
+ <!-- FOR IE9 below -->
+ <!--[if lt IE 9]>
+ <script src="js/respond.min.js"></script>
+ <![endif]-->
+
+
+</head>
+<body>
+    <div id="colorlib-page">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="colorlib-navbar-brand">
+							<a class="colorlib-logo" href="index.html"><span>NE</span><span>LY</span></a>
+						</div>
+				</div>
+			</div>
+            <br>
+            <br>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="rotate">
+                        <h2 class="heading">Portofolio</h2>
+                    </div>
+                </div>
+
+
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/nail1.jpg" class="card-img-top" alt="Skyscrapers"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Nails Art</h3>
+                          <p class="card-text">
+                            Nail art is a creative way to paint, decorate, enhance, and embellish nails.
+                            Many techniques, shapes, and design are available in nail art.
+                          </p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/nail2.jpg" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Nails Art</h3>
+                          <p class="card-text">Nail art is a creative way to paint, decorate, enhance, and embellish nails.
+                            Many techniques, shapes, and design are available in nail art.</p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/nail3.jpg" class="card-img-top" alt="Palm Springs Road"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Nails Art</h3>
+                          <p class="card-text">
+                            Nail art is a creative way to paint, decorate, enhance, and embellish nails.
+                  Many techniques, shapes, and design are available in nail art.
+                          </p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                <br>
+                  <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/hair1.jpg" class="card-img-top" alt="Skyscrapers"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Hair Do</h3>
+                          <p class="card-text">
+                            Hairdo is an informal word for "hairstyle". That's how your hair is styled, and beautified with hairpiece.
+                          </p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/hair2.jpg" class="card-img-top" alt="Los Angeles Skyscrapers"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Hair Do</h3>
+                          <p class="card-text">Hairdo is an informal word for "hairstyle". That's how your hair is styled, and beautified with hairpiece.</p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="card h-100">
+                        <img src="/img/hair3.jpg" class="card-img-top" alt="Palm Springs Road"/>
+                        <div class="card-body">
+                          <h3 class="card-title">Hair Do</h3>
+                          <p class="card-text">
+                            Hairdo is an informal word for "hairstyle". That's how your hair is styled, and beautified with hairpiece.
+                          </p>
+                        </div>
+                        <div class="card-footer">
+                          <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+
+            <footer>
+                <div id="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-pb-sm">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <h2>Let's Talk</h2>
+                                        <p>Contact me to get started</p>
+                                        <p><a href="#">nely@info.com</a></p>
+                                        <p class="colorlib-social-icons">
+                                            <a href="#"><i class="icon-facebook4"></i></a>
+                                            <a href="#"><i class="icon-twitter3"></i></a>
+                                            <a href="#"><i class="icon-googleplus"></i></a>
+                                            <a href="#"><i class="icon-dribbble2"></i></a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-pb-sm">
+                                <h2>Latest Job</h2>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-1.jpg);"></a>
+                                    <div class="desc">
+                                        <span>August 15, 2022</span>
+                                        <h3><a href="#">Art Gallery in Bandung</a></h3>
+                                    </div>
+                                </div>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-2.jpg);"></a>
+                                    <div class="desc">
+                                        <span>September 18, 2022</span>
+                                        <h3><a href="#">A Japanese Theater</a></h3>
+                                    </div>
+                                </div>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-3.jpg);"></a>
+                                    <div class="desc">
+                                        <span>September 24, 2022</span>
+                                        <h3><a href="#">Wedding MUA</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-pb-sm">
+                                <h2>For More Updates</h2>
+                                <p>Just drop your email, Girls.</p>
+                                <div class="subscribe text-center">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control text-center" placeholder="Enter Email address">
+                                        <input type="submit" value="Subscribe" class="btn btn-primary btn-custom">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <p>
+                                    &copy;
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> NELY ANDRIANY</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+ <script src="/js/app.js"></script>
+ <script src="/js/my-style.js"></script>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,226 @@
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Profile</title>
+
+ <link rel="stylesheet" href="/css/app.css">
+ <link rel="stylesheet" href="/css/my-style.css">
+
+ <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+ <link rel="shortcut icon" href="favicon.ico">
+
+ <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700" rel="stylesheet">
+
+ <!-- Animate.css -->
+ <link rel="stylesheet" href="css/animate.css">
+ <!-- Icomoon Icon Fonts-->
+ <link rel="stylesheet" href="css/icomoon.css">
+ <!-- Bootstrap  -->
+ <link rel="stylesheet" href="css/bootstrap.css">
+ <!-- Owl Carousel -->
+ <link rel="stylesheet" href="css/owl.carousel.min.css">
+ <link rel="stylesheet" href="css/owl.theme.default.min.css">
+ <!-- Magnific Popup -->
+ <link rel="stylesheet" href="css/magnific-popup.css">
+
+ <link rel="stylesheet" href="css/style.css">
+
+
+ <!-- Modernizr JS -->
+ <script src="js/modernizr-2.6.2.min.js"></script>
+ <!-- FOR IE9 below -->
+ <!--[if lt IE 9]>
+ <script src="js/respond.min.js"></script>
+ <![endif]-->
+
+
+</head>
+<body>
+    <div id="colorlib-page">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="colorlib-navbar-brand">
+							<a class="colorlib-logo" href="index.html"><span>NE</span><span>LY</span></a>
+						</div>
+				</div>
+			</div>
+
+            <div id="colorlib-about">
+                <div class="container">
+                    <div class="row text-center">
+                        <h2 class="bold">About</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4 d-flex align-items-center justify-content-center about-img">
+                            <img src="/img/foto.png" class="img-fluid" alt="">
+                          </div>
+                        <div class="col-md-8 col-md-push-1">
+                            <div class="about-desc">
+                                    <div class="item">
+                                        <h2>Hi i'm Nely! </h2>
+                                    </div>
+                                    <div class="item">
+                                        <h3>I'm A Beauty Enthusiast</h3>
+                                    </div>
+                                </div>
+                                <div class="desc">
+                                    <div class="rotate">
+                                        <h2 class="heading">About</h2>
+                                    </div>
+                                <p>A beauty enthusiast of hairdo and nail art. Starting from my admiration, obsession and love for colorfull painting and decorating with a beads
+                                    and now the beauty of art has become the inspiration to create loveliness on nails and hair. </p>
+
+                                    <p><a href="#" class="btn btn-primary btn-outline">Contact Me!</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+<section id="services" class="services section-bg">
+    <div class="container">
+
+      <div class="section-title" data-aos="fade-up">
+        <h1><center><bold>Services</bold></center></h2>
+        <p><center>Check out the great services we offer</center></p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-check"></i></div>
+            <h1 class="title"><a href="">Package 1</a></h1>
+            <p class="description">
+                <h3><b>Manicure</b></h3>
+              <ul>
+                <li><h4>On Natural nails</h4></li>
+                <li><h4> Include gel polish</h4></li>
+              </ul>
+
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-check"></i></div>
+            <h1 class="title"><a href="">Package 2</a></h1>
+            <p class="description">
+              <h3><b>Manicure + Fake nails</b></h3>
+              <ul>
+                <li><h4>On fake nails</h4></li>
+                <li><h4>Include gel polish</h4></li>
+              </ul>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-check"></i></div>
+            <h1 class="title"><a href="">Package 3</a></h1>
+            <p class="description">
+              <h3><b>Manicure + Gel Extensions</b></h3>
+              <ul>
+                <li><h4>On gel extensions</h4></li>
+                <li><h4>Include gel polish</h4></li>
+              </ul>
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-3 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="400">
+          <div class="icon-box">
+            <div class="icon"><i class="bx bx-check"></i></div>
+            <h2 class="title"><a href="">Package Hair Do</a></h2>
+            <p class="description">
+              <h3><b>Vitamin + Styling</b></h3>
+              <ul>
+                <li><h4>By all request</h4></li>
+                <li><h4>Include hair piece</h4></li>
+              </ul>
+            </p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+            <footer>
+                <div id="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4 col-pb-sm">
+                                <div class="row">
+                                    <div class="col-md-10">
+                                        <h2>Let's Talk</h2>
+                                        <p>Contact me to get started</p>
+                                        <p><a href="#">nely@info.com</a></p>
+                                        <p class="colorlib-social-icons">
+                                            <a href="#"><i class="icon-facebook4"></i></a>
+                                            <a href="#"><i class="icon-twitter3"></i></a>
+                                            <a href="#"><i class="icon-googleplus"></i></a>
+                                            <a href="#"><i class="icon-dribbble2"></i></a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-pb-sm">
+                                <h2>Latest Job</h2>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-1.jpg);"></a>
+                                    <div class="desc">
+                                        <span>August 15, 2022</span>
+                                        <h3><a href="#">Art Gallery in Bandung</a></h3>
+                                    </div>
+                                </div>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-2.jpg);"></a>
+                                    <div class="desc">
+                                        <span>September 18, 2022</span>
+                                        <h3><a href="#">A Japanese Theater</a></h3>
+                                    </div>
+                                </div>
+                                <div class="f-entry">
+                                    <a href="#" class="featured-img" style="background-image: url(images/img-3.jpg);"></a>
+                                    <div class="desc">
+                                        <span>September 24, 2022</span>
+                                        <h3><a href="#">Wedding MUA</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-pb-sm">
+                                <h2>For More Updates</h2>
+                                <p>Just drop your email, Girls.</p>
+                                <div class="subscribe text-center">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control text-center" placeholder="Enter Email address">
+                                        <input type="submit" value="Subscribe" class="btn btn-primary btn-custom">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <p>
+                                    &copy;
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> NELY ANDRIANY</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+ <script src="/js/app.js"></script>
+ <script src="/js/my-style.js"></script>
+</body>
+</html>
